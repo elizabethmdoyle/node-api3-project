fc0d417f1bc224e06c05f5ed41acc3cfac25eb5c
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-02-19 15:07:50 -0500

    15 out fo 20 passing tests

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -31,15 +31,21 @@ router.post('/',validateUser, (req, res, next) => {
    User.insert({name: req.name })
       .then(newUser => {
 
-        res.statusMessage(201).json(newUser)
+        res.status(201).json(newUser)
       })
       .catch(next)
 });
 
-router.put('/:id',  validateUserId, validateUser, (req, res) => {
+router.put('/:id',  validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  User.update(req.params.id, {name: req.name})
+    .then(updatedUser => {
+      res.json(updatedUser)
+    })
+    .catch(next)
+      
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
